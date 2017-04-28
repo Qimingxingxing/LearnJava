a1a2012220be090f3ee40f431373dfb20fa7235c
qimingxingxing <qz718@nyu.edu>
2017-04-28 17:37:21 -0400

    update notes

---
+++ WaitNotify.java
@@ -0,0 +1,29 @@
+package MultiThread;
+
+/**
+ * Created by qimingzhang on 2017/4/28.
+ */
+public class WaitNotify extends Thread{
+    int total;
+    @Override
+    public void run(){
+        synchronized (this){
+            for(int i=0;i<100;i++){
+                total=total+1;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        WaitNotify countThread=new WaitNotify();
+        countThread.start();
+        synchronized (countThread){
+            System.out.println("等到countThread线程计算结束...");
+            try {
+                countThread.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("计算的结果是："+countThread.total);
+        }
+    }
+}
