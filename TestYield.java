7bb77b81d84733a553708eeea5b621ec95b70151
qimingxingxing <qz718@nyu.edu>
2017-04-28 15:55:15 -0400

    update notes

---
+++ TestYield.java
@@ -0,0 +1,23 @@
+package MultiThread;
+
+/**
+ * Created by qimingzhang on 2017/4/28.
+ */
+public class TestYield implements Runnable{
+    @Override
+    public void run() {
+        for(int i=0;i<10;++i){
+            System. out.println(Thread.currentThread().getName()+ "运行"+i);
+            if(i==3){
+                System. out.println("线程的礼让" );
+                Thread. yield();
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Thread h1= new Thread(new TestYield(),"A");
+        Thread h2= new Thread(new TestYield(),"B");
+        h1.start();
+        h2.start();
+    }
+}
