046c0678f6356abc642abb42d1664b8d78211dd0
qimingxingxing <qz718@nyu.edu>
2017-04-28 18:21:40 -0400

    add executors

--- ExecutorsTest.java
+++ ExecutorsTest.java
@@ -1,13 +1,12 @@
 package MultiThread;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
  * Created by qimingzhang on 2017/4/28.
  */
 public class ExecutorsTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService executorService = Executors.newFixedThreadPool(10);
 
         executorService.execute(new Runnable() {
@@ -16,6 +15,25 @@ public class ExecutorsTest {
             }
         });
 
+
+        Future future = executorService.submit(new Runnable() {
+            public void run() {
+                System.out.println("Asynchronous task");
+            }
+        });
+
+        future.get();  //returns null if the task has finished correctly.
+
+        future = executorService.submit(new Callable(){
+            public Object call() throws Exception {
+                System.out.println("Asynchronous Callable");
+                return "Callable Result";
+            }
+        });
+
+        System.out.println("future.get() = " + future.get());
+
         executorService.shutdown();
+
     }
 }
