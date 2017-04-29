ea680d0a286926e82702f607660106f0acb656ee
qimingxingxing <qz718@nyu.edu>
2017-04-28 21:10:51 -0400

    add Exception handle

--- ExceptionTest.java
+++ ExceptionTest.java
@@ -1,10 +1,13 @@
 package MultiThread;
 
+import java.io.IOException;
+
 /**
  * Created by qimingzhang on 2017/4/28.
  */
 public class ExceptionTest {
     public static void main(String[] args) {
+        /*
         while(!Thread.currentThread().isInterrupted()){
             try{
                 Thread.currentThread().interrupt();
@@ -16,6 +19,15 @@ public class ExceptionTest {
                 Thread.currentThread().interrupt();
             }
         }
+        */
+        try {
+            new ExceptionTest().method();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public void method()throws Exception{
+        throw new IOException();
     }
 }
 //http://www.cnblogs.com/onlywujun/p/3565082.html
