d7ae134429a817c2e5b0a1c8cbdeaa4231b82bf0
qimingxingxing <qz718@nyu.edu>
2017-04-28 20:00:32 -0400

    add executors

---
+++ ThreadPoolTest.java
@@ -0,0 +1,26 @@
+package MultiThread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by qimingzhang on 2017/4/28.
+ */
+public class ThreadPoolTest {
+    public static void main(String[] args) {
+        int  corePoolSize  =    5;
+        int  maxPoolSize   =   10;
+        long keepAliveTime = 5000;
+
+        ExecutorService threadPoolExecutor =
+                new ThreadPoolExecutor(
+                        corePoolSize,
+                        maxPoolSize,
+                        keepAliveTime,
+                        TimeUnit.MILLISECONDS,
+                        new LinkedBlockingQueue<Runnable>()
+                );
+    }
+}
