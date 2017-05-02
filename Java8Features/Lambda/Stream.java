20e8fbbe23dc5a248d987a0ff0ee38021aa691a5
qimingxingxing <qz718@nyu.edu>
2017-05-02 16:56:41 -0400

    add optional

--- Java8Features/Lambda/Stream.java
+++ Java8Features/Lambda/Stream.java
@@ -1,10 +1,20 @@
 package LearnJava.Java8Features.Lambda;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by qimingzhang on 2017/5/2.
  */
 public class Stream {
     public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(2);
+        list.add(2);
 
+        System.out.println(list.parallelStream().distinct().count());
     }
 }
