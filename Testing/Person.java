7a6b11fde73707c152b6dcbb17b157967bf224e2
qimingxingxing <qz718@nyu.edu>
2017-05-04 00:04:24 -0400

    add junit

---
+++ Testing/Person.java
@@ -0,0 +1,16 @@
+package LearnJava.Testing;
+
+/**
+ * Created by qimingzhang on 2017/5/3.
+ */
+public class Person {
+    private final String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
