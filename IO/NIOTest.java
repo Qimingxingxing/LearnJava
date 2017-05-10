1a24cef8cc6be084a627cc0d26d253dd64d830cb
qimingxingxing <qz718@nyu.edu>
2017-05-10 14:06:39 -0400

    add nio

--- IO/NIOTest.java
+++ IO/NIOTest.java
@@ -6,6 +6,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.SelectableChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Created by qimingzhang on 2017/5/10.
@@ -37,5 +42,29 @@ public class NIOTest {
         outChannel.close();
         fi.close();
         fo.close();
+    }
+    public void handle(SelectableChannel ... channels) throws IOException{
+        Selector selector = Selector.open();
+        for (SelectableChannel channel: channels){
+            channel.register(selector, SelectionKey.OP_WRITE | SelectionKey.OP_READ);
+        }
+        while (!Thread.currentThread().isInterrupted()){
+            int readyChannels = selector.select();
+            if (readyChannels == 0){
+                continue;
+            }
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            Iterator<SelectionKey> keyIterator = selectionKeys.iterator();
+            while (keyIterator.hasNext()){
+                SelectionKey key = keyIterator.next();
+                SelectableChannel channel = key.channel();
+                if(key.isReadable()){
+                    // TODO
+                }else if(key.isWritable()){
+                    // TODO
+                }
+                keyIterator.remove();
+            }
+        }
     }
 }
